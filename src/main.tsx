31205c776e581d283e102656797576df7250f182
Joseph (Joey) Cristina <hi@kvvpa.com>
2026-05-08 02:50:01 -0700

    Load Slice Board planning layout overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './AppSliceBoard'
 import './index.css'
+import './slice-board-overrides.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
