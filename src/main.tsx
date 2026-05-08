e92ae8b31307335826f557059119d66c342a296b
Joseph (Joey) Cristina <hi@kvvpa.com>
2026-05-08 07:28:56 -0700

    Run multi-pie Slice Board app

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppSliceBoardCalloutsV2'
+import App from './AppSliceBoardMultiPie'
 import './index.css'
 import './slice-board-overrides.css'
 import './slice-board-callouts.css'
+import './slice-board-multi-pie.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
