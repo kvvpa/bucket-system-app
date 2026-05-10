86c06d1ea8502f497c04702ee8a78254986675a3
Joseph (Joey) Cristina <hi@kvvpa.com>
2026-05-09 18:45:07 -0700

    Load clean VNext mobile fit styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './slice-board-overrides.css'
 import './slice-board-callouts.css'
 import './slice-board-multi-pie.css'
 import './slice-board-vnext.css'
+import './slice-board-vnext-mobile.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
